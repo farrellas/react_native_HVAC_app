1a04c0f020f53598a17683cc9b7855735f55b6a1
Andrew Farrell <farrellas05@yahoo.com>
2022-05-15 19:00:24 -0600

    first commit

--- App.tsx
+++ App.tsx
@@ -4,8 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* Task Checklist */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>To be completed</Text>
+
+        <View style={styles.items}>
+          {/* Tasks start here */}
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -13,8 +21,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop:80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {},
 });
---
+++ README.md
@@ -0,0 +1,1 @@
+"# react_native_HVAC_app" 
